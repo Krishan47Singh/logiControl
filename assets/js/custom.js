d78d9e49f486b48df104cc382370ed34e287233e
Krishan47Singh <krishansingh5922@gmail.com>
2025-06-14 14:19:58 +0530

    change in slidebar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,32 @@
+const toggler = document.querySelector(".toggler-icon");
+const sidebar = document.querySelector(".slide-navbar");
+const closeBtn = document.querySelector(".close");
 
+toggler.addEventListener("click", (e) => {
+  e.stopPropagation();
+  sidebar.style.transform = "translateX(0)";
+});
 
-  const toggler = document.querySelector('.toggler-icon');
-  const sidebar = document.querySelector('.slide-navbar');
-  const closeBtn = document.querySelector('.close');
+closeBtn.addEventListener("click", () => {
+  sidebar.style.transform = "translateX(800px)";
+});
 
-  
-  toggler.addEventListener('click', (e) => {
-    e.stopPropagation();
-    sidebar.style.transform = 'translateX(0)';
-  });
+document.addEventListener("click", (e) => {
+  const isClickInsideSidebar = sidebar.contains(e.target);
+  const isClickOnToggler = toggler.contains(e.target);
+  if (!isClickInsideSidebar && !isClickOnToggler) {
+    sidebar.style.transform = "translateX(800px)";
+  }
+});
 
+// navbar sticky
 
-  closeBtn.addEventListener('click', () => {
-    sidebar.style.transform = 'translateX(650px)';
-  });
+let nav = document.querySelector(".navbar");
 
-  document.addEventListener('click', (e) => {
-    const isClickInsideSidebar = sidebar.contains(e.target);
-    const isClickOnToggler = toggler.contains(e.target);
-    if (!isClickInsideSidebar && !isClickOnToggler) {
-      sidebar.style.transform = 'translateX(650px)';
-    }
-  });
-
-
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 600) {
+    nav.classList.add("fixedbg");
+  } else {
+    nav.classList.remove("fixedbg");
+  }
+});
